9f4655cc7e88394bb699988703fa9645067e7913
ghalibse <ghalibse@gmail.com>
2016-09-12 19:15:26 -0400

    First Commit

---
+++ app/src/main/java/com/libu/simplewebview/MainActivity.java
@@ -0,0 +1,84 @@
+package com.libu.simplewebview;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private WebView mWebView;
+    private EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.main_txtView);
+        mWebView = (WebView) findViewById(R.id.main_WebView);
+        mEditText = (EditText) findViewById(R.id.main_txt);
+
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+//            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+
+            mTextView.setBackgroundColor(Color.GREEN);
+            mTextView.setText(toast);
+
+        }
+    }
+
+    public void doMagic(View view) {
+
+        mWebView.loadUrl("http://www.mocky.io/v2/57d73501100000f50fa06e1b");
+    }
+
+
+}
